536321b2d6c3a09c73d3a6e4adb08766ba935388
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-01-23 22:57:57 -0500

    update index.js: load the configuration variables

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,1 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
+
+// Import the configuration settings for the current stack.
+const config = new pulumi.Config();
+const appPath = config.require('appPath');
+const prefixName = config.require('prefixName');
+const imageName = prefixName;
+const imageTag = config.require('imageTag');
+
+// Azure container instances service does not support port mapping
+// so, the containerPort and pubicPort must be the same
+const containerPort = config.requireNumber('containerPort');
+const publicPort = config.requireNumber('publicPort');
+const cpu = config.requireNumber('cpu');
+const memory = config.requireNumber('memory');
+

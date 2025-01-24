e582a054052f48b87be9e36674a287d6e6a6ea6e
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-01-24 18:32:55 -0500

    update index.js

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,9 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from '@pulumi/azure-native/resources';
 import * as containerregistry from '@pulumi/azure-native/containerregistry';
 
-import * as docker from '@pulumi/docker';
-import * as containerinstance from '@pulumi/azure-native/containerinstance';
-
 // Import the configuration settings for the current stack.
 const config = new pulumi.Config();
 const appPath = config.require('appPath');
@@ -43,77 +40,3 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
-
-  const image = new docker.Image(`${prefixName}-image`, {
-    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
-    build: {
-      context: appPath,
-      platform: 'linux/amd64',
-    },
-    registry: {
-      server: registry.loginServer,
-      username: registryCredentials.username,
-      password: registryCredentials.password,
-    },
-  })
-
-  const containerGroup = new containerinstance.ContainerGroup(
-    `${prefixName}-container-group`,
-    {
-      resourceGroupName: resourceGroup.name,
-      osType: 'linux',
-      restartPolicy: 'always',
-      imageRegistryCredentials: [
-        {
-          server: registry.loginServer,
-          username: registryCredentials.username,
-          password: registryCredentials.password,
-        },
-      ],
-      containers: [
-        {
-          name: imageName,
-          image: image.imageName,
-          ports: [
-            {
-              port: containerPort,
-              protocol: 'tcp',
-            },
-          ],
-          environmentVariables: [
-            {
-              name: 'PORT',
-              value: containerPort.toString(),
-            },
-            {
-              name: 'WEATHER_API_KEY',
-              value: '8a8b44a428b8afd66bf312ca4c49c25c',
-            },
-          ],
-          resources: {
-            requests: {
-              cpu: cpu,
-              memoryInGB: memory,
-            },
-          },
-        },
-      ],
-      ipAddress: {
-        type: containerinstance.ContainerGroupIpAddressType.Public,
-        dnsNameLabel: `${imageName}`,
-        ports: [
-          {
-            port: publicPort,
-            protocol: 'tcp',
-          },
-        ],
-      },
-    },
-  )
-
-// Export the service's IP address, hostname, and fully-qualified URL.
-export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
-export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
-export const url = containerGroup.ipAddress.apply(
-  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
-)
